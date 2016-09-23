cd8d9ba01d28a4bbe3d9a18eeb8fa1f5a5d2f1ae
袁超 <yuanchao@syswin.com>
2016-09-23 14:39:14 +0800

    functionPointer

---
+++ 21_functionPointer/21_functionPointer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  21_functionPointer
+//  函数指针
+//  Created by yuanchao on 2016/9/22.
+//  Copyright © 2016年 yuanchao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
