fa1366ace07bd3cf2ca67a30477a89f67fb8bb79
袁超 <yuanchao@syswin.com>
2016-09-27 16:56:16 +0800

    add function pointer code

--- 21_functionPointer/21_functionPointer/main.c
+++ 21_functionPointer/21_functionPointer/main.c
@@ -8,8 +8,21 @@
 
 #include <stdio.h>
 
+void hello(int a,int b,char * c){
+    printf("Hello World\n");
+}
+
+typedef void(*simpleFunc)();
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    
+    // 定义函数指针 指向函数内存地址
+//    void(*fp)(int,int,char) = &hello;
+    
+   simpleFunc fp = &hello;
+    
+    fp(0,0,"x");
+    
     return 0;
 }
